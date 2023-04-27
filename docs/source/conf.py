5ff6f4c318c1dffc27a5bfa4fb924fd34d3f45cf
Davide Laghi <davide.laghi01@gmail.com>
2023-04-27 12:44:28 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary'
 ]
-autosummary_generate = True # Turn on sphinx.ext.autosummary
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
