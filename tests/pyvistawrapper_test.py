8e8eae9c8c4be323bcd2e339ad4d513ed32dca40
Davide Laghi <davide.laghi01@gmail.com>
2023-05-04 17:28:22 +0200

    legacy tests implemented

--- tests/pyvistawrapper_test.py
+++ tests/pyvistawrapper_test.py
@@ -1,5 +1,6 @@
 import os
 from importlib.resources import files, as_file
+import pytest
 
 from f4enix.output.pyvistawrap import PyVistaWrapper
 import tests.resources.pyvistawrapper.tests as res
@@ -10,87 +11,29 @@ expected = files(res_exp)
 
 
 class TestPyVistaWrapper:
-    def test_example_csv(self, tmpdir):
-        with as_file(resources.joinpath('example.vts')) as inp:
+
+    @pytest.mark.parametrize(
+            ['file_read', 'list_array_names', 'out_format', 'out_name', 'file_exp'],
+            [['example.vts', None, 'csv', 'example_csv.csv', "example_['Values']_csv.csv"],
+             ['test_VTK_CUBE_SQUARE.vtr', ["Value - Total"], 'csv', 'test_VTK_CUBE_SQUARE_csv.csv', "test_VTK_CUBE_SQUARE_['Value - Total']_csv.csv"],
+             ['meshtal_14.vts', ["Value - Total"], 'csv', 'meshtal_14_csv.csv', "meshtal_14_['Value - Total']_csv.csv"],
+             ['cuvmsh_44_CuV_CELF10.vtr', ["Value - Total"], 'csv', 'cuvmsh_44_CuV_CELF10_csv.csv', "cuvmsh_44_CuV_CELF10_['Value - Total']_csv.csv"],
+             ['PS_NHD_DIV_RHC_INBOARD.vtk', ["NHD[W/cm3]"], 'csv', 'PS_NHD_DIV_RHC_INBOARD_csv.csv', "PS_NHD_DIV_RHC_INBOARD_['NHD[W-cm3]']_csv.csv"],
+             ['PS_NHD_DIV_RHC_INBOARD.vtk', ["NHD[W/cm3]"], 'ip_fluent', 'PS_NHD_DIV_RHC_INBOARD_ip_fluent.txt', "PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_ip_fluent.txt"],
+             ['PS_NHD_DIV_RHC_INBOARD.vtk', ["NHD[W/cm3]"], 'point_cloud', 'PS_NHD_DIV_RHC_INBOARD_point_cloud.txt', "PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_point_cloud.txt"]
+            ]
+            )
+    def test_write(self, file_read, list_array_names, out_format, out_name,
+                   file_exp, tmpdir):
+        with as_file(resources.joinpath(file_read)) as inp:
             mesh = PyVistaWrapper.from_file(inp)
 
         outpath = tmpdir.mkdir('sub_csv')
-        mesh.write_mesh(outpath, out_format='csv')
-        outfile = os.path.join(outpath, 'example_csv.csv')
+        mesh.write_mesh(outpath, out_format=out_format,
+                        list_array_names=list_array_names)
+        outfile = os.path.join(outpath, out_name)
 
-        with as_file(expected.joinpath("example_['Values']_csv.csv")) as exp:
+        with as_file(expected.joinpath(file_exp)) as exp:
             with open(outfile, "r") as test, open(exp, 'r') as exp_file:
                 for line1, line2 in zip(test, exp_file):
                     assert line1 == line2
-
-    # def test_vtk_cube_square_csv(self):
-    #     functions.open_mesh("tests/data/test_VTK_CUBE_SQUARE.vtr")
-    #     functions.write_mesh("tests/data/test_VTK_CUBE_SQUARE.vtr", ["Value - Total"], "csv")
-    #     with open(
-    #         "tests/data/expected_results/test_VTK_CUBE_SQUARE_['Value - Total']_csv.csv", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/test_VTK_CUBE_SQUARE_['Value - Total']_csv.csv", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/test_VTK_CUBE_SQUARE_['Value - Total']_csv.csv")
-    #     self.assertEqual(expected, result)
-
-    # def test_meshtal_14_csv(self):
-    #     functions.open_mesh("tests/data/meshtal_14.vts")
-    #     functions.write_mesh("tests/data/meshtal_14.vts", ["Value - Total"], "csv")
-    #     with open(
-    #         "tests/data/expected_results/meshtal_14_['Value - Total']_csv.csv", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/meshtal_14_['Value - Total']_csv.csv", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/meshtal_14_['Value - Total']_csv.csv")
-    #     self.assertEqual(expected, result)
-
-    # def test_cuvmsh_44_celf10_csv(self):
-    #     functions.open_mesh("tests/data/cuvmsh_44_CuV_CELF10.vtr")
-    #     functions.write_mesh("tests/data/cuvmsh_44_CuV_CELF10.vtr", ["Value - Total"], "csv")
-    #     with open(
-    #         "tests/data/expected_results/cuvmsh_44_CuV_CELF10_['Value - Total']_csv.csv", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/cuvmsh_44_CuV_CELF10_['Value - Total']_csv.csv", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/cuvmsh_44_CuV_CELF10_['Value - Total']_csv.csv")
-    #     self.assertEqual(expected, result)
-
-    # def test_rhc_inboard_csv(self):
-    #     functions.open_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk")
-    #     functions.write_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk", ["NHD[W/cm3]"], "csv")
-    #     with open(
-    #         "tests/data/expected_results/PS_NHD_DIV_RHC_INBOARD_['NHD[W-cm3]']_csv.csv", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/PS_NHD_DIV_RHC_INBOARD_['NHD[W-cm3]']_csv.csv", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/PS_NHD_DIV_RHC_INBOARD_['NHD[W-cm3]']_csv.csv")
-    #     self.assertEqual(expected, result)
-
-    # def test_rhc_inboard_ip_fluent(self):
-    #     functions.open_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk")
-    #     functions.write_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk", ["NHD[W/cm3]"], "ip_fluent")
-    #     with open(
-    #         "tests/data/expected_results/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_ip_fluent.txt", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_ip_fluent.txt", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_ip_fluent.txt")
-    #     self.assertEqual(expected, result)
-
-    # def test_rhc_inboard_point_cloud(self):
-    #     functions.open_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk")
-    #     functions.write_mesh("tests/data/PS_NHD_DIV_RHC_INBOARD.vtk", ["NHD[W/cm3]"], "point_cloud")
-    #     with open(
-    #         "tests/data/expected_results/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_point_cloud.txt", "r"
-    #     ) as infile:
-    #         expected = infile.read()
-    #     with open("tests/data/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_point_cloud.txt", "r") as infile:
-    #         result = infile.read()
-    #     os.remove("tests/data/PS_NHD_DIV_RHC_INBOARD_NHD[W-cm3]_point_cloud.txt")
-    #     self.assertEqual(expected, result)
