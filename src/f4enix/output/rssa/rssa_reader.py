0aa0fdffc4442777a072780930a284519731e0dd
AlvaroCubi <55387701+AlvaroCubi@users.noreply.github.com>
2026-02-10 15:38:17 +0100

    RSSA scanning

--- src/f4enix/output/rssa/rssa_reader.py
+++ src/f4enix/output/rssa/rssa_reader.py
@@ -1,7 +1,10 @@
+from collections.abc import Iterator
+from pathlib import Path
 from typing import BinaryIO
 
 import numpy as np
 import polars as pl
+from polars.io.plugins import register_io_source
 
 from f4enix.output.rssa.rssa_helpers import (
     BYTE,
@@ -12,6 +15,8 @@ from f4enix.output.rssa.rssa_helpers import (
     FileParameters,
     SurfaceParameters,
 )
+
+BYTES_PER_TRACK = 96
 
 SCHEMA = pl.Schema(
     {
@@ -30,7 +35,94 @@ SCHEMA = pl.Schema(
 )
 
 
-def parse_header(infile: BinaryIO) -> FileParameters:
+def parse_header(path: Path | str) -> FileParameters:
+    """Reads the header of the RSSA file and returns the file parameters as a
+    FileParameters object."""
+    with open(path, "rb") as infile:
+        return _parse_header_binary(infile)
+
+
+def parse_tracks(path: Path | str) -> pl.DataFrame:
+    """Reads the tracks recorded in the RSSA file and returns them as a polars
+    DataFrame."""
+    with open(path, "rb") as infile:
+        # Skip the header
+        _ = _parse_header_binary(infile)
+        return _parse_tracks_binary(infile)
+
+
+def scan_rssa_file(
+    path_to_file: Path | str, default_batch_size: int = 100_000_000
+) -> pl.LazyFrame:
+    """Scans the RSSA to return a polars LazyFrame instead of a DataFrame.
+
+    Parameters
+    ----------
+    path_to_file: Path | str
+        The RSSA file to read.
+    default_batch_size: int
+        The default batch size to use when reading the file in chunks. This is the
+        number of tracks to read at once. The optimal value depends on the size of the
+        file and the available memory, but a good starting point is around 100 million
+        tracks (~8 Gb).
+
+    Examples
+    --------
+    >>> from f4enix.output.rssa import RSSA
+    >>> from f4enix.output.rssa.rssa_reader import parse_header, scan_rssa_file
+    >>> parameters = parse_header('small_cyl.w')
+    >>> lazy_tracks = scan_rssa_file('small_cyl.w')
+    >>> # We will only read the first 20 tracks of the file
+    >>> tracks = lazy_tracks.head(20).collect()
+    >>> rssa = RSSA(parameters, tracks)
+    """
+
+    def source_generator(
+        with_columns: list[str] | None,
+        predicate: pl.Expr | None,
+        n_rows: int | None,
+        batch_size: int | None,
+    ) -> Iterator[pl.DataFrame]:
+        """
+        Generator function that creates the source.
+        This function will be registered as IO source.
+        """
+        if batch_size is None:
+            batch_size = default_batch_size
+
+        # Initialize the reader
+        with open(path_to_file, "rb") as reader:
+            # Skip the header
+            _ = _parse_header_binary(reader)
+
+            while n_rows is None or n_rows > 0:
+                if n_rows is not None:
+                    batch_size = min(batch_size, n_rows)
+
+                number_of_bytes_to_read = (
+                    batch_size * BYTES_PER_TRACK
+                )  # each particle record is 96 bytes long
+
+                df = _parse_tracks_binary(reader, number_of_bytes_to_read)
+
+                if df.shape[0] == 0:
+                    break
+
+                if n_rows is not None:
+                    n_rows -= df.shape[0]
+
+                # Apply the column selection and predicate filtering if provided
+                if with_columns is not None:
+                    df = df.select(with_columns)
+                if predicate is not None:
+                    df = df.filter(predicate)
+
+                yield df
+
+    return register_io_source(io_source=source_generator, schema=SCHEMA)
+
+
+def _parse_header_binary(infile: BinaryIO) -> FileParameters:
     first_record = _read_fortran_record(infile)
     # The first line of the file with information like the code version, date and title
     formatted_record_id = first_record.tobytes().decode("UTF-8")
@@ -105,9 +197,11 @@ def parse_header(infile: BinaryIO) -> FileParameters:
     )
 
 
-def parse_tracks(file: BinaryIO) -> pl.DataFrame:
+def _parse_tracks_binary(
+    file: BinaryIO, number_of_bytes_to_read: int = -1
+) -> pl.DataFrame:
     # Read the whole remaining of the file at once, store all the bytes as a 1D np array
-    data = np.fromfile(file, BYTE)
+    data = np.fromfile(file, BYTE, number_of_bytes_to_read)
 
     # Reshape the array so each index holds the information of a single particle
     # we can do this because we know that the particle records have always the same
